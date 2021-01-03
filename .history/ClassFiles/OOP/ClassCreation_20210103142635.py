2c44b92227fc6306315d67ea7c8571212909f1d8
minefarmer <pgoldfarm@yahoo.com>
2021-01-03 15:11:12 -0600

    test

---
+++ .history/ClassFiles/OOP/ClassCreation_20210103142635.py
@@ -0,0 +1,22 @@
+'''                 Creating a class
+Created with the keyword class followed by a name,
+Common practice is to make the names Pascal Casing:  Example: MyFirstCar
+A class consists of variables(Attributes) and functions (Methods)
+Classes can be used to model a lot of things.
+
+
+
+
+'''
+class Instructors:
+    companyName = "Bluelime"  # I've defined the class on this line.
+
+    def __init__(self,course):  # __init__ is a built-in function. It is called the initializer
+        self.course = course  # I've created the variable.
+    
+    def printinfo(self):
+        print("My company name is ", Instructors.companyName)
+
+class Pets:
+    pass
+print()
---
+++ ClassFiles/OOP/InstanciatingAClass.py
@@ -0,0 +1,4 @@
+'''                     instantiating a class
+
+
+'''
